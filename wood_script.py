1a8056120b00f9b7fb6c7b9972716f532776a9b4
Anastasia Egorova <anastasego@workmac-2.local>
2019-11-11 14:35:21 +0300

    Added pretty rounding with Decimal

--- wood_script.py
+++ wood_script.py
@@ -1,8 +1,11 @@
 # The second part of task one - with **
+from decimal import Decimal
+
+
 def task_part_two(input_dict, output_m3):
     # Just bringing it to a format that's a little more readable & easier to process
     m3_cost_input_dict = {item['m3']: item['cost'] for item in input_dict}
-    return {m3: sum(m3_cost_input_dict.values())*m3/sum(output_m3) for m3 in output_m3}
+    return {m3: Decimal(str(round(sum(m3_cost_input_dict.values())*m3/sum(output_m3), 2))) for m3 in output_m3}
 
 
 if __name__ == "__main__":
@@ -10,7 +13,10 @@ if __name__ == "__main__":
     cost_in = [113.67, 213.5, 317.22, 522.3, 325.14]
     m3_out = [1.1, 0.313, 0.467, 1.2, 0.951, 1.113, 1.363]
     # Taking sum of the initial cost and evenly distributing it onto the output m3 values
-    cost_out = {m3: sum(cost_in)*m3/sum(m3_out) for m3 in m3_out}
+    cost_out = {m3: Decimal(str(round(sum(cost_in)*m3/sum(m3_out), 2))) for m3 in m3_out}
     print(cost_out)
-    result = task_part_two([{'m3': 1.33, 'cost': 200.1}, {'m3': 2.33, 'cost': 300.1}], [1, 2.5])
+    task_2_input = [{'m3': 1.33, 'cost': 200.1}, {'m3': 2.33, 'cost': 300.1}]
+    result = task_part_two(task_2_input, [1, 2.5])
     print(result)
+    print(sum(result.values()))
+    print(sum(result.values()) == sum({item['m3']: item['cost'] for item in task_2_input}.values()))
