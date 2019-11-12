97299336867987bda9748f1e188c9fbe5c226ccb
Anastasia Egorova <anastasego@workmac-2.local>
2019-11-12 15:08:15 +0300

    Reorganized calculations

--- wood_script.py
+++ wood_script.py
@@ -2,25 +2,35 @@ from decimal import Decimal
 import pandas as pd
 
 
+# Because division doesn't always divide without loss, especially when dealing with rounded values
+def reassign_sums_difference(output_costs, input_sum, output_sum):
+    sum_difference = input_sum - output_sum
+    if sum_difference == 0:
+        return output_costs
+    cent = Decimal(str(0.01)) if sum_difference > 0 else Decimal(str(-0.01))
+    difference_in_cents = abs(int(sum_difference / cent))
+    iter_difference = iter([cent] * difference_in_cents)
+    # Just randomly reassign it to items. It can be redone to add difference to the biggest or the smallest costs etc
+    output_costs = output_costs.apply(lambda cost: cost + next(iter_difference, 0))
+    return output_costs
+
+
 # The second part of task one - with **
 def task_part_two(input_dict, output_m3):
     input_df = pd.DataFrame(input_dict)
     output_df = pd.DataFrame().assign(m3=output_m3)
+    output_df['m3'] = output_df['m3'].apply(lambda x: Decimal(str(round(x, 3))))
+    input_df['m3'] = input_df['m3'].apply(lambda x: Decimal(str(round(x, 3))))
+    input_df['cost'] = input_df['cost'].apply(lambda x: Decimal(str(round(x, 2))))
     output_df['cost'] = output_df.apply(lambda row:
                                         Decimal(str(round(input_df['cost'].sum()*row['m3']/output_df['m3'].sum(), 2))),
                                         axis=1)
+    output_df['cost'] = reassign_sums_difference(output_df['cost'], input_df['cost'].sum(), output_df['cost'].sum())
     return output_df.to_dict('records')
 
 
 if __name__ == "__main__":
-    # task 1.1
-    m3_in = [1.323, 0.561, 2.312, 1.567, 0.997]
-    cost_in = [113.67, 213.5, 317.22, 522.3, 325.14]
-    m3_out = [1.1, 0.313, 0.467, 1.2, 0.951, 1.113, 1.363]
-    # Taking sum of the initial cost and evenly distributing it onto the output m3 values
-    cost_out = {m3: Decimal(str(round(sum(cost_in)*m3/sum(m3_out), 2))) for m3 in m3_out}
-    print(cost_out)
-    # Task 1.2
-    task_2_input = [{'m3': 1.33, 'cost': 200.1}, {'m3': 2.33, 'cost': 300.1}]
-    result = task_part_two(task_2_input, [1, 2.5])
+    input_structure = [{'m3': 31.302, 'cost': 1000.01}]
+    result = task_part_two(input_structure, [3.478 for _ in range(6)])
     print(result)
+    print(sum([a['cost'] for a in result]))
